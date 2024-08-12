a93ccebf42d02d427ccc35a3224e20e48eb3dc2b
s4b1n0 <sabino.heitor@hotmail.com>
2024-08-12 06:26:32 -0300

    data cleaning pronto

--- src/dataCleaning.py
+++ src/dataCleaning.py
@@ -1,11 +1,40 @@
-import subprocess
+from bs4 import BeautifulSoup
 
-# Definindo a URL e o nome do arquivo de saída
-url = "https://www.amazon.com.br/Viol%C3%A3o-Strinberg-Sd200c-El%C3%A9trico-Tabaco/dp/B07VLJKRLX/ref=sr_1_13?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3NIO4ALR5XLN1&dib=eyJ2IjoiMSJ9.mUIS7BZ4Bf5rZKesecoWE2TC8VzUSYtUk0dvHITOiPPfLYgAWNu7t-EyifWmj4i1pinpu19cE4b4xZwu_H-5DvFLIMcu3oNS9acJwdScGvDTRIhpsTSjuGbGzHVmZegLVDw2emoeWyuVnQEusClLR6emaQW6VyagGDOQ6_7qgHcBl6ivEDw0RwqCGyEUwuDnMQ-u7HZzUadrALwqwA29J9rWx1gvlN3SjOwBZAKliLaKt5JXVn6hbpLD_CWdIZhaJpDjwmdFUt34o-6hTxoKC_Cgpx1bknzCgYHSPbiI0k4.K2w1nd1nLsmYNyzt4RM9SpZtBv9jbIh6LAXKnXFxWhM&dib_tag=se&keywords=violao&qid=1723300079&sprefix=violao%2Caps%2C210&sr=8-13&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
-output_file = "page.html"
+# Nome do arquivo HTML
+nome_arquivo = "src/pageCompras.html"
 
-# Comando curl para baixar a página
-command = ["curl", "-sL", url, "-o", output_file]
+# Ler o conteúdo do arquivo HTML
+with open(nome_arquivo, 'r', encoding='utf-8') as file:
+    html = file.read()
 
-# Executando o comando
-subprocess.run(command, check=True)
+# Criar o objeto BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+# Encontrar todos os blocos de produtos
+produtos = soup.find_all('div', class_='promocao-produtos-item')
+
+# Lista para armazenar os objetos de produtos
+lista_produtos = []
+
+# Iterar sobre cada produto e extrair as informações
+for produto in produtos:
+    # Extrair o nome do produto
+    produto_nome = produto.find('div', class_='promocao-item-nome').get_text(strip=True)
+    
+    # Extrair o preço do produto (em dólares)
+    produto_preco = produto.find('div', class_='promocao-item-preco-oferta').find('strong').get_text(strip=True)
+    
+    # Extrair o nome da loja
+    loja_nome = produto.find('div', class_='promocao-item-preco-oferta promocao-item-border flex column').find('img', class_='lozad')['alt']
+    
+    # Criar um dicionário para o produto e adicionar à lista
+    produto_obj = {
+        "nome": produto_nome,
+        "preco": produto_preco,
+        "loja": loja_nome
+    }
+    lista_produtos.append(produto_obj)
+
+# Exibir a lista de produtos
+for produto in lista_produtos:
+    print(produto)
--- src/getPage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-URL="https://www.amazon.com.br/Viol%C3%A3o-Strinberg-Sd200c-El%C3%A9trico-Tabaco/dp/B07VLJKRLX/ref=sr_1_1_sspa?crid=WZ7B7MGK51WQ&dib=eyJ2IjoiMSJ9.T4BUxlHDLFwtM23Xf8WQc50BuPSglfPZQpqCb-TC0cnoFJKCtR_a8bFG7dNSiWyCHkMvN0o2aCJt-F0d7SWrJrJl0znMnuj352bUqdeNMvNznGJxUQA3yzeaSHU3O2LKZq5lnECQlCSb7ivGgzth9-wy21zFugSALhIiAEy4ZtwgTxIiz1cX8yj76oQ3-vzoxuJQydYixwJVfjOK0WK-IskxPmlkoFCUTJXuvtIqXkqntTWq57TzawP2fPaNqcfUws2FIptoUtE9cSt_ZUeQJRFlKSv0pqrAVI8UyeCWurg.fHkM-dMHP9LGTb20w4iFBocXBOl4og0bI6MhlkwezWY&dib_tag=se&keywords=viol%C3%A3o+strinberg+forest+fs4d&qid=1723334725&sprefix=viol%C3%A3o+strinberg+for%2Caps%2C194&sr=8-1-spons&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
-
-wget $URL
