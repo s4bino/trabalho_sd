806a6949dc1e39e5efe3ceb09433f22796e3f8c7
s4b1n0 <sabino.heitor@hotmail.com>
2024-08-12 23:33:14 -0300

    scheduler pronto no python

--- src/dataCleaning.py
+++ src/dataCleaning.py
@@ -1,41 +1,52 @@
 from bs4 import BeautifulSoup
 import json
+import sched
+import time
+import requests
 
-# Nome do arquivo HTML
-nome_arquivo = "src/pageCompras.html"
+# Função de limpeza de dados
+def dataCleaning():
+    nome_arquivo = "src/pageCompras.html"
 
-# Ler o conteúdo do arquivo HTML
-with open(nome_arquivo, 'r', encoding='utf-8') as file:
-    html = file.read()
+    with open(nome_arquivo, 'r', encoding='utf-8') as file:
+        html = file.read()
 
-# Criar o objeto BeautifulSoup
-soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
+    produtos = soup.find_all('div', class_='promocao-produtos-item')
+    lista_produtos = []
 
-# Encontrar todos os blocos de produtos
-produtos = soup.find_all('div', class_='promocao-produtos-item')
+    for produto in produtos:
+        produto_nome = produto.find('div', class_='promocao-item-nome').get_text(strip=True)
+        produto_preco = produto.find('div', class_='promocao-item-preco-oferta').find('strong').get_text(strip=True)
+        loja_nome = produto.find('div', class_='promocao-item-border').find('img', class_='lozad')['alt']
 
-# Lista para armazenar os objetos de produtos
-lista_produtos = []
+        produto_obj = {
+            "nome": produto_nome,
+            "preco": produto_preco,
+            "loja": loja_nome
+        }
+        lista_produtos.append(produto_obj)
 
-# Iterar sobre cada produto e extrair as informações
-for produto in produtos:
-    # Extrair o nome do produto
-    produto_nome = produto.find('div', class_='promocao-item-nome').get_text(strip=True)
-    
-    # Extrair o preço do produto (em dólares)
-    produto_preco = produto.find('div', class_='promocao-item-preco-oferta').find('strong').get_text(strip=True)
-    
-    # Extrair o nome da loja
-    loja_nome = produto.find('div', class_='promocao-item-preco-oferta promocao-item-border flex column').find('img', class_='lozad')['alt']
-    
-    # Criar um dicionário para o produto e adicionar à lista
-    produto_obj = {
-        "nome": produto_nome,
-        "preco": produto_preco,
-        "loja": loja_nome
-    }
-    lista_produtos.append(produto_obj)
+    return lista_produtos
 
-# Convertendo o dicionário para JSON e salvando em um arquivo
-with open("dados.json", "w") as arquivo_json:
-    json.dump(lista_produtos, arquivo_json, indent=4)
+# Configuração do scheduler
+scheduler = sched.scheduler(time.time, time.sleep)
+
+def agendar(intervalo):
+    lista_produtos = dataCleaning()
+    enviar(lista_produtos)
+    scheduler.enter(intervalo, 1, agendar, (intervalo,))
+
+def enviar(lista_produtos):
+    # Envie a requisição POST para o servidor
+    response = requests.post('http://localhost:3000/api/produtos', json=lista_produtos)
+
+    # Imprima a resposta do servidor
+    print(response.text)
+
+# Intervalo de tempo entre execuções em segundos
+intervalo = 5
+
+# Iniciar o agendamento
+scheduler.enter(0, 1, agendar, (intervalo,))
+scheduler.run()
--- src/enviar.py
+++ src/enviar.py
@@ -1,12 +1,2 @@
-import requests
-import json
 
-# Carregue o arquivo JSON
-with open('dados.json', 'r') as f:
-    data = json.load(f)
 
-# Envie a requisição POST para o servidor
-response = requests.post('http://localhost:3000/api/produtos', json=data)
-
-# Imprima a resposta do servidor
-print(response.text)
